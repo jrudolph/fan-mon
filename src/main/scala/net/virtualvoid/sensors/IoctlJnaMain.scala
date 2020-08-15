213ed28b3b8aa2350811e414b66acc470ae04df3
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-08-15 21:29:01 +0200

    some fan speed / temp "API"

--- src/main/scala/net/virtualvoid/sensors/IoctlJnaMain.scala
+++ src/main/scala/net/virtualvoid/sensors/IoctlJnaMain.scala
@@ -2,20 +2,40 @@ package net.virtualvoid.sensors
 
 import java.io.{ FileDescriptor, RandomAccessFile }
 
-object IoctlJnaMain extends App {
-  val f = new RandomAccessFile("/dev/tuxedo_cc_wmi", "r")
+case class FanInfo(
+    speed:              Double,
+    temperatureCelsius: Byte
+)
+trait ClevoFan {
+  def fanInfo(): FanInfo
+}
+object ClevoFan {
+  def apply(): ClevoFan = new ClevoFan {
+    val tuxedoWmiFd: Int = {
+      val f = new RandomAccessFile("/dev/tuxedo_cc_wmi", "r")
+      val fdField = classOf[RandomAccessFile].getDeclaredField("fd")
+      fdField.setAccessible(true)
+      val fd = fdField.get(f)
+      val fdFdField = classOf[FileDescriptor].getDeclaredField("fd")
+      fdFdField.setAccessible(true)
+      fdFdField.get(fd).asInstanceOf[Int]
+    }
+    override def fanInfo(): FanInfo = {
+      val bytes = new Array[Byte](8)
+      val res = jna.Ioctl.ioctl(tuxedoWmiFd, 0x8008ec10L, bytes)
+      require(res == 0, s"Ioctl result was != 0: $res")
+      FanInfo(
+        (bytes(0) & 0xff).toDouble / 256,
+        bytes(1)
+      )
+    }
+  }
+}
 
-  val fdField = classOf[RandomAccessFile].getDeclaredField("fd")
-  fdField.setAccessible(true)
-  val fd = fdField.get(f)
-  val fdFdField = classOf[FileDescriptor].getDeclaredField("fd")
-  fdFdField.setAccessible(true)
-  val fdInt = fdFdField.get(fd).asInstanceOf[Int]
-
-  println(s"Hello World $fd $fdInt")
-
-  val ptrJna = new Array[Byte](8)
-  val res = jna.Ioctl.ioctl(fdInt, 0x8008ec10L, ptrJna)
-  println(f"Result: $res ptr: ${ptrJna.map(_ formatted "%02x").mkString}%s")
-
+object FanMonitor extends App {
+  val fan = ClevoFan()
+  Iterator.continually {
+    Thread.sleep(500)
+    fan.fanInfo()
+  }.take(10).foreach(println)
 }
