55f4d91248bffdc0d5c039609af77e769905de1a
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-08-16 21:33:36 +0200

    double height graph lines and many more sensors

--- src/main/scala/net/virtualvoid/sensors/FanMonitor.scala
+++ src/main/scala/net/virtualvoid/sensors/FanMonitor.scala
@@ -69,10 +69,43 @@ class FanMonitor(ioctl: (Int, Long, Array[Byte]) => Int) {
     val minValue = sensor.minValueOption.getOrElse(values.min)
     val maxValue = sensor.maxValueOption.getOrElse(values.max)
     val span = maxValue - minValue
-    def toBar(value: Double): Char = Ansi.bars(((value - minValue) * 5 / span).toInt.max(0).min(4))
+    def toBar(value: Double): Char = Ansi.bars(((value - minValue) * 6 / span).toInt.max(0).min(5))
     val latest = values.last
     val bars = values.map(toBar).mkString
     f"${sensor.label.take(12)}%-12s ${bars formatted "%-" + barWidth + "s"}%s $latest%7.1f ${sensor.unit}%s\n"
+  }
+
+  def historyLineDouble(history: mutable.Seq[Double], sensor: Sensor): String = {
+    def color(level: Int): String = {
+      if (level < 4) Console.GREEN
+      else if (level < 9) Console.YELLOW
+      else Console.RED
+    }
+
+    val values =
+      if (sensor.isCumulative)
+        history.takeRight(barWidth + 1).sliding(2).map {
+          case ArrayBuffer(i0: Double, i1: Double) => i1 - i0
+          case ArrayBuffer(i0: Double)             => 0 // FIXME
+        }.toVector
+      else history.takeRight(barWidth)
+    val minValue = sensor.minValueOption.getOrElse(values.min)
+    val maxValue = sensor.maxValueOption.getOrElse(values.max)
+    val span = maxValue - minValue
+    def toBar0(value: Double): String = {
+      val level = ((value - minValue) * 12 / span).toInt
+      s"${color(level)}${Ansi.bars((level - 6).max(0).min(5))}"
+    }
+    def toBar1(value: Double): String = {
+      val level = ((value - minValue) * 12 / span).toInt
+      s"${color(level)}${Ansi.bars(level.max(0).min(5))}"
+    }
+    val latest = values.last
+    val bars0 = values.map(toBar0).mkString
+    val bars1 = values.map(toBar1).mkString
+
+    f"${""}%-12s $bars0%s${Console.RESET}\n" +
+      f"${sensor.label.take(12)}%-12s $bars1%s${Console.RESET} $latest%7.1f ${sensor.unit}%s\n"
   }
 
   case class Sensor(
@@ -85,13 +118,17 @@ class FanMonitor(ioctl: (Int, Long, Array[Byte]) => Int) {
   )
 
   val fanSpeed = Sensor("Fan speed", () => fan.fanInfo().speed * 100, Some(0), Some(100d), "%")
-  val fanTemp = Sensor("Fan temp", () => fan.fanInfo().temperatureCelsius, Some(30), Some(105), "°C")
-  val packageEnergy = Sensor("CPU energy", () => readEnergyCumulativeMicroJoule() / 1000, Some(0), Some(40000), "mW", isCumulative = true)
+  val fanTemp = Sensor("Fan temp", () => fan.fanInfo().temperatureCelsius, Some(55), Some(105), "°C")
+  val packageEnergy = Sensor("Pkg energy", () => readEnergyCumulativeMicroJoule() / 1000, Some(0), Some(40000), "mW", isCumulative = true)
+  val coreEnergy = Sensor("Core energy", () => readCoreEnergyCumulativeMicroJoule() / 1000, Some(0), Some(40000), "mW", isCumulative = true)
+  val uncoreEnergy = Sensor("Uncore energy", () => readUncoreEnergyCumulativeMicroJoule() / 1000, Some(0), Some(40000), "mW", isCumulative = true)
+  def coreThermalThrottlePackageCountSensor(core: Int): Sensor =
+    Sensor(s"Core $core Throttle", () => thermalThrottlePackageCount(core)() / 1000, None, None, "", isCumulative = true)
   def coreFreqSensor(core: Int): Sensor =
     Sensor(s"Core $core freq", () => coreFreq(core)() / 1000, Some(1200), Some(4000), "MHz")
 
   val sensors = Seq(
-    fanSpeed, fanTemp, packageEnergy
+    fanSpeed, fanTemp, packageEnergy, coreEnergy, uncoreEnergy, coreThermalThrottlePackageCountSensor(0)
   ) ++ (0 to 3).map(coreFreqSensor)
 
   val data: Seq[(Sensor, ArrayBuffer[Double])] = sensors.map(_ -> new ArrayBuffer[Double]())
@@ -112,19 +149,28 @@ class FanMonitor(ioctl: (Int, Long, Array[Byte]) => Int) {
     print(Ansi.EraseDisplayBelow)
 
     data.foreach {
-      case (s, b) => print(historyLine(b, s))
+      case (s, b) => print(historyLineDouble(b, s))
     }
 
-    print(s"\u001b[${sensors.size}A")
+    print(s"\u001b[${sensors.size * 2}A")
 
     Thread.sleep(1000)
   }
 
   def coreFreq(core: Int): () => Long =
-    readLongFromFile(s"/sys/devices/system/cpu/cpu${core}/cpufreq/scaling_cur_freq")
+    readLongFromFile(s"/sys/devices/system/cpu/cpu$core/cpufreq/scaling_cur_freq")
 
   def readEnergyCumulativeMicroJoule: () => Long =
     readLongFromFile("/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/energy_uj")
+
+  def readCoreEnergyCumulativeMicroJoule: () => Long =
+    readLongFromFile("/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/intel-rapl:0:0/energy_uj")
+
+  def readUncoreEnergyCumulativeMicroJoule: () => Long =
+    readLongFromFile("/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/intel-rapl:0:1/energy_uj")
+
+  def thermalThrottlePackageCount(core: Int): () => Long =
+    readLongFromFile(s"/sys/devices/system/cpu/cpu$core/thermal_throttle/package_throttle_count")
 
   def readLongFromFile(fileName: String): () => Long =
     () => Source.fromFile(fileName).getLines().next().toLong // FIXME: crappy implementation
